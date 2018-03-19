3fafad11b0aa3d4b1fd60a2fa69bd184af8fcf34
gapprapp <puckcheetu56@gmail.com>
2018-03-19 14:24:47 +0700

    Initial commit

---
+++ platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.telerik.plugins.nativepagetransitions.NativePageTransitions",
+    "file": "plugins/com.telerik.plugins.nativepagetransitions/www/NativePageTransitions.js",
+    "pluginId": "com.telerik.plugins.nativepagetransitions",
+    "clobbers": [
+      "window.plugins.nativepagetransitions"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "com.telerik.plugins.nativepagetransitions": "0.6.5"
+};
+// BOTTOM OF METADATA
+});
