e5dc87e098351f254c627a67904eb05a6a19f11a
mewole <37533052+mewole@users.noreply.github.com>
2018-03-27 23:12:27 +0700

    tran_in

---
+++ platforms/ios/www/php/transfer_in.php
@@ -0,0 +1,44 @@
+<?php 
+    $conn = mysqli_connect("localhost", "id3340019_dd", "pkl2468GG", "id3340019_dd");    
+    $input  = $_POST['JSON'];
+    $obj = json_decode($input,true);  	
+    $shop_id  = $_POST['shop_id'];   
+    $dt  = $_POST['date']; 
+    $i = 0;
+
+    mysqli_autocommit($conn,FALSE); 
+    $sql = "INSERT INTO transfer_in (date_time,shop_id) VALUE ('$dt','$shop_id')"; 
+    $result = mysqli_query($conn, $sql); 
+    $last_id = mysqli_insert_id($conn);	
+   
+    foreach ($obj as $data)
+    {
+        $i++;
+        $item = $i;
+        $prod_id = $data['prod_id'];         
+        $amt = $data['amt'];  
+
+        $sql_item = "INSERT INTO transfer_in_item (tran_id,item_id,prod_id,amount) VALUE ('$last_id','$item','$prod_id','$amt')"; 
+        $result_item = mysqli_query($conn, $sql_item);
+        
+        $query = "SELECT prod_id FROM shop WHERE shop_id = '$shop_id' AND prod_id = '$prod_id'";
+        $result_q = mysqli_query($conn, $query);
+        if(mysqli_num_rows($result_q) > 0){ 
+            $sql_up = "UPDATE shop SET amount = amount+'$amt' WHERE shop_id = '$shop_id' AND prod_id = '$prod_id'"; 
+            $result_up = mysqli_query($conn, $sql_up);
+        }else{
+            $sql_in = "INSERT INTO shop (shop_id,prod_id,amount) VALUE ('$shop_id','$prod_id','$amt')"; 
+            $result_in = mysqli_query($conn, $sql_in);
+        }              
+    }
+
+    if($result){
+        echo "success";
+        mysqli_commit($conn);          
+    }else{
+        echo "fail";
+        mysqli_rollback($conn);
+    }
+
+    mysqli_close($conn);
+?>
