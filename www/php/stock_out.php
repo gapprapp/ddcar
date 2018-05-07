f5417f654c473893eb083b18c7860f351840775d
mewole <37533052+mewole@users.noreply.github.com>
2018-05-07 21:41:17 +0700

    9.41

--- www/php/stock_out.php
+++ www/php/stock_out.php
@@ -3,44 +3,43 @@
     $input  = $_POST['JSON'];
     $obj = json_decode($input,true);  	
     $ware_id  = $_POST['ware_id'];   
-    $dt  = $_POST['date']; 
-    $sum  = $_POST['sum']; 
+    $dt  = $_POST['date'];   
     $last_id = "";
     $i = 0;
 
     mysqli_autocommit($conn,FALSE);
-    $sql = "SELECT stock_number,count FROM stock_in ORDER BY stock_id DESC LIMIT 1"; 
+    $sql = "SELECT stock_out_number,count FROM stock_out ORDER BY stock_out_id DESC LIMIT 1"; 
     $result = mysqli_query($conn, $sql);   
   
     if(mysqli_num_rows($result) > 0){    
         while($row = mysqli_fetch_array($result)){  
-            $number = $row['stock_number'];
+            $number = $row['stock_out_number'];
             $year_old = substr($number,3,2);
             $year_cur = date("y");
             if($year_cur == $year_old){
                 $count = $row['count']+1;
-                $year = "IMP" . $year_cur . "-";
+                $year = "EXP" . $year_cur . "-";
                 $stock_number = $year . str_pad($count, 5, "0",STR_PAD_LEFT);
-                $sql2 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
-                VALUES ('$stock_number','$dt','$ware_id','$sum','$count')"; 
+                $sql2 = "INSERT INTO stock_out (stock_out_number,date_time,ware_id,count) 
+                VALUES ('$stock_number','$dt','$ware_id','$count')"; 
                 $result2 = mysqli_query($conn, $sql2);
                 $last_id = mysqli_insert_id($conn);	
                 //echo "normal"; 
             }else{
-                $year = "IMP" . $year_cur . "-";
+                $year = "EXP" . $year_cur . "-";
                 $stock_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
-                $sql3 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
-                VALUES ('$stock_number','$dt','$ware_id','$sum',1)"; 
+                $sql3 = "INSERT INTO stock_out (stock_number,date_time,ware_id,count) 
+                VALUES ('$stock_number','$dt','$ware_id',1)"; 
                 $result3 = mysqli_query($conn, $sql3);
                 $last_id = mysqli_insert_id($conn);	
                 //echo "year dif"; 
             }           
         }
     }else{
-        $year = "IMP" . date("y") . "-";
+        $year = "EXP" . date("y") . "-";
         $stock_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
-        $sql1 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
-        VALUES ('$stock_number','$dt','$ware_id','$sum',1)"; 
+        $sql1 = "INSERT INTO stock_out (stock_number,date_time,ware_id,count) 
+        VALUES ('$stock_number','$dt','$ware_id',1)"; 
         $result1 = mysqli_query($conn, $sql1);
         $last_id = mysqli_insert_id($conn);	
         //echo "empty"; 
@@ -53,19 +52,11 @@
         $cost = $data['cost'];    
         $amt = $data['amt'];  
 
-        $sql_item = "INSERT INTO stock_in_item (stock_id,item_id,prod_id,stock_cost,amount) VALUE ('$last_id','$item','$prod_id','$cost','$amt')"; 
-        $result_item = mysqli_query($conn, $sql_item);
-        
-        $query = "SELECT prod_id FROM warehouse WHERE ware_id = '$ware_id' AND prod_id = '$prod_id'";
-        $result_q = mysqli_query($conn, $query);
-        if(mysqli_num_rows($result_q) > 0){ 
-            $sql_up = "UPDATE warehouse SET amount = amount+'$amt' WHERE ware_id = '$ware_id' AND prod_id = '$prod_id'"; 
-            $result_up = mysqli_query($conn, $sql_up);
-        }else{
-            $sql_in = "INSERT INTO warehouse (ware_id,prod_id,amount) VALUE ('$ware_id','$prod_id','$amt')"; 
-            $result_in = mysqli_query($conn, $sql_in);
-        }       
-               
+        $sql_item = "INSERT INTO stock_out_item (stock_out_id,item_id,prod_id,stock_out_cost,amount) VALUE ('$last_id','$item','$prod_id','$cost','$amt')"; 
+        $result_item = mysqli_query($conn, $sql_item);       
+       
+        $sql_up = "UPDATE warehouse SET amount = amount-'$amt' WHERE ware_id = '$ware_id' AND prod_id = '$prod_id'"; 
+        $result_up = mysqli_query($conn, $sql_up);            
     }
 
     if($result){
