5b05c85f1fc12158d366ab55e0eb2dce02f124a5
mewole <37533052+mewole@users.noreply.github.com>
2018-05-07 22:43:34 +0700

    eiei

--- platforms/android/app/src/main/assets/www/php/stock_in.php
+++ platforms/android/app/src/main/assets/www/php/stock_in.php
@@ -4,6 +4,7 @@
     $obj = json_decode($input,true);  	
     $ware_id  = $_POST['ware_id'];   
     $dt  = $_POST['date']; 
+    $sum  = $_POST['sum']; 
     $last_id = "";
     $i = 0;
 
@@ -20,16 +21,16 @@
                 $count = $row['count']+1;
                 $year = "IMP" . $year_cur . "-";
                 $stock_number = $year . str_pad($count, 5, "0",STR_PAD_LEFT);
-                $sql2 = "INSERT INTO stock_in (stock_number,date_time,ware_id,count) 
-                VALUES ('$stock_number','$dt','$ware_id','$count')"; 
+                $sql2 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
+                VALUES ('$stock_number','$dt','$ware_id','$sum','$count')"; 
                 $result2 = mysqli_query($conn, $sql2);
                 $last_id = mysqli_insert_id($conn);	
                 //echo "normal"; 
             }else{
                 $year = "IMP" . $year_cur . "-";
                 $stock_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
-                $sql3 = "INSERT INTO stock_in (stock_number,date_time,ware_id,count) 
-                VALUES ('$stock_number','$dt','$ware_id',1)"; 
+                $sql3 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
+                VALUES ('$stock_number','$dt','$ware_id','$sum',1)"; 
                 $result3 = mysqli_query($conn, $sql3);
                 $last_id = mysqli_insert_id($conn);	
                 //echo "year dif"; 
@@ -38,8 +39,8 @@
     }else{
         $year = "IMP" . date("y") . "-";
         $stock_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
-        $sql1 = "INSERT INTO stock_in (stock_number,date_time,ware_id,count) 
-        VALUES ('$stock_number','$dt','$ware_id',1)"; 
+        $sql1 = "INSERT INTO stock_in (stock_number,date_time,ware_id,total_stock,count) 
+        VALUES ('$stock_number','$dt','$ware_id','$sum',1)"; 
         $result1 = mysqli_query($conn, $sql1);
         $last_id = mysqli_insert_id($conn);	
         //echo "empty"; 
