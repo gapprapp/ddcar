0283cda3192583f3f4f503912d11d51b6861010a
mewole <37533052+mewole@users.noreply.github.com>
2019-05-12 17:07:35 +0700

    apk

--- platforms/ios/www/php/save_order.php
+++ platforms/ios/www/php/save_order.php
@@ -19,8 +19,7 @@
     $user_id = $_POST['user_id'];
 
     mysqli_begin_transaction($conn);
-    foreach ($obj_d as $data)
-    {        
+    foreach ($obj_d as $data){
         $prod_id = $data['prod_id'];
         $price = $data['price'];   
       
@@ -38,10 +37,80 @@
             exit;
         }        
     }
+
+    $credit_id = 0;
+    if(strpos($pay,'เครดิต') !== false){
+        $lastday = date("Y-m-t", strtotime($dt));
+        $firstday = date("Y-m-01", strtotime($dt));
+        $sql = "SELECT * FROM credit WHERE date_time BETWEEN '$firstday' AND '$lastday' AND cus_id = '$cus'"; 
+        $result = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($result) > 0){    
+            while($row = mysqli_fetch_array($result)){  
+                $credit_id = $row['credit_id'];                
+            } 
+            $sql = "UPDATE credit SET credit_price = credit_price+'$total' WHERE credit_id = '$credit_id'";
+            $result = mysqli_query($conn, $sql);
+            if(!$result){
+                mysqli_rollback($conn);
+                echo "fail";
+                exit;
+            }    
+        }else{
+            $sql = "SELECT credit_num,count FROM credit ORDER BY credit_id DESC LIMIT 1"; 
+            $result = mysqli_query($conn, $sql);    
+        
+            if(mysqli_num_rows($result) > 0){    
+                while($row = mysqli_fetch_array($result)){  
+                    $number = $row['credit_num'];
+                    $year_old = substr($number,1,2);
+                    $year_cur = date("y");
+                    if($year_cur == $year_old){
+                        $count = $row['count']+1;
+                        $year = "C" . $year_cur . "-";
+                        $order_number = $year . str_pad($count, 5, "0",STR_PAD_LEFT);
+                        $sql2 = "INSERT INTO credit (credit_num,cus_id,credit_price,date_time,count) 
+                        VALUE ('$order_number','$cus','$total','$dt','$count')"; 
+                        $result2 = mysqli_query($conn, $sql2);
+                        if(!$result2){
+                            mysqli_rollback($conn);
+                            echo "fail";
+                            exit;
+                        }
+                        $credit_id = mysqli_insert_id($conn);                
+                    }else{
+                        $year = "C" . $year_cur . "-";
+                        $order_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
+                        $sql3 = "INSERT INTO credit (credit_num,cus_id,credit_price,date_time,count) 
+                        VALUE ('$order_number','$cus','$total','$dt',1)"; 
+                        $result3 = mysqli_query($conn, $sql3);
+                        if(!$result3){
+                            mysqli_rollback($conn);
+                            echo "fail";
+                            exit;
+                        }
+                        $credit_id = mysqli_insert_id($conn);               
+                    }           
+                }
+            }else{
+                $year = "C" . date("y") . "-";
+                $order_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
+                $sql1 = "INSERT INTO credit (credit_num,cus_id,credit_price,date_time,count) 
+                VALUE ('$order_number','$cus','$total','$dt',1)"; 
+                $result1 = mysqli_query($conn, $sql1);
+                if(!$result1){
+                    mysqli_rollback($conn);
+                    echo "fail";
+                    exit;
+                }
+                $credit_id = mysqli_insert_id($conn);       
+            }	
+        }
+    }
+
     $sql = "SELECT order_number,count FROM sale_order ORDER BY order_id DESC LIMIT 1"; 
     $result = mysqli_query($conn, $sql);    
    
-    if(mysqli_num_rows($result) > 0){    
+    if(mysqli_num_rows($result) > 0){
         while($row = mysqli_fetch_array($result)){  
             $number = $row['order_number'];
             $year_old = substr($number,1,2);
@@ -51,8 +120,8 @@
                 $year = "S" . $year_cur . "-";
                 $order_number = $year . str_pad($count, 5, "0",STR_PAD_LEFT);
                 $sql2 = "INSERT INTO sale_order (order_number,customer_id,date_time,sum_price,
-                payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,count) 
-                VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id','$count')"; 
+                payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,credit_id,count) 
+                VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id','$credit_id','$count')"; 
                 $result2 = mysqli_query($conn, $sql2);
                 if(!$result2){
                     mysqli_rollback($conn);
@@ -64,8 +133,8 @@
                 $year = "S" . $year_cur . "-";
                 $order_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
                 $sql3 = "INSERT INTO sale_order (order_number,customer_id,date_time,sum_price,
-                payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,count) 
-                VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id',1)"; 
+                payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,credit_id,count) 
+                VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id','$credit_id',1)"; 
                 $result3 = mysqli_query($conn, $sql3);
                 if(!$result3){
                     mysqli_rollback($conn);
@@ -79,8 +148,8 @@
         $year = "S" . date("y") . "-";
         $order_number = $year . str_pad(1, 5, "0",STR_PAD_LEFT);
         $sql1 = "INSERT INTO sale_order (order_number,customer_id,date_time,sum_price,
-        payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,count) 
-        VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id',1)"; 
+        payment_type,branch_id,type,total_discount,total_price,get_price,chng,user_id,credit_id,count) 
+        VALUE ('$order_number','$cus','$dt','$sum','$pay','$b','$type','$dis','$total','$get','$change','$user_id','$credit_id',1)"; 
         $result1 = mysqli_query($conn, $sql1);
         if(!$result1){
             mysqli_rollback($conn);
@@ -91,8 +160,7 @@
     }  
     $item = 0;
     $ch = 'false'; 
-    foreach ($obj as $data)
-    {
+    foreach ($obj as $data){
         $item++;
         $prod_id = $data['prod_id'];
         $price = $data['price'];    
