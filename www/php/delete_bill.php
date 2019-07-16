3cead713f16c9fec54733a27e9ad2f06a7370316
mewole <37533052+mewole@users.noreply.github.com>
2019-07-16 13:56:46 +0700

    ลบบิล แก้ไขบิลในมือถือ

--- www/php/delete_bill.php
+++ www/php/delete_bill.php
@@ -3,7 +3,10 @@
     $bill_id  = $_POST['bill_id'];
     $branch_id  = $_POST['b_id'];
     $type  = $_POST['type'];
-    $txt = "(cancel)";  
+    $pay  = $_POST['pay'];
+    $total  = $_POST['total'];
+    $credit_id  = $_POST['credit_id'];
+    $txt = "(cancel)";
 
     mysqli_begin_transaction($conn);
     $query = "SELECT prod_amount,prod_id FROM sale_order_item WHERE order_id = '$bill_id'";  
@@ -13,11 +16,11 @@
             $amt = $row['prod_amount']; 
             $prod_id = $row['prod_id']; 
             if($type == "โกดัง"){
-                $query = "UPDATE warehouse SET amount=amount+'$amt' WHERE ware_id = '$branch_id' AND prod_id = '$prod_id'";  
+                $query1 = "UPDATE warehouse SET amount=amount+'$amt' WHERE ware_id = '$branch_id' AND prod_id = '$prod_id'";  
             }else if($type == "หน้าร้าน"){
-                $query = "UPDATE shop SET amount=amount+'$amt' WHERE shop_id = '$branch_id' AND prod_id = '$prod_id'";  
+                $query1 = "UPDATE shop SET amount=amount+'$amt' WHERE shop_id = '$branch_id' AND prod_id = '$prod_id'";  
             }            
-            $result1 = mysqli_query($conn, $query); 
+            $result1 = mysqli_query($conn, $query1); 
             if(!$result1){
                 mysqli_rollback($conn);
                 echo "fail";
@@ -32,7 +35,18 @@
         mysqli_rollback($conn);
         echo "fail";
         exit;
-    }        
+    }
+    
+    if($pay != "เงินสด"){
+        $sql = "UPDATE credit SET credit_price = credit_price-'$total' WHERE credit_id = '$credit_id'";  
+        $result = mysqli_query($conn, $sql); 
+        if(!$result){
+            mysqli_rollback($conn);
+            echo "fail";
+            exit;
+        }
+    }
+   
     mysqli_commit($conn);
     echo "success"; 
     mysqli_close($conn);
