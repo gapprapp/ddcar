6f52e44fdaa23bd31bd4ecf84511c2c256109a5e
mewole <37533052+mewole@users.noreply.github.com>
2019-09-19 22:06:09 +0700

    fix credit and profit in web

--- www/php/get_shift_bill.php
+++ www/php/get_shift_bill.php
@@ -5,13 +5,30 @@
     $date_from = $date.' 00:00:00';
     $date_to = $date.' 23:59:59';
     $txt = "(cancel)";
-    
-    $query = "SELECT s.order_id,s.order_number,u.user_name,s.total_price FROM sale_order s INNER JOIN user u
+    $cost = 0;
+
+    $query = "SELECT s.order_id,s.order_number,u.user_name,s.total_price,s.count FROM sale_order s INNER JOIN user u
     ON s.user_id = u.user_id WHERE s.date_time BETWEEN '$date_from' AND '$date_to' AND s.order_number NOT LIKE '%$txt%'";    
     $result = mysqli_query($conn, $query);
 
     if(mysqli_num_rows($result) > 0){    
-      while($row = mysqli_fetch_array($result)){      
+      while($row = mysqli_fetch_array($result)){
+        $order_id = $row['order_id'];
+        $query1 = "SELECT prod_id FROM sale_order_item WHERE order_id = '$order_id'";    
+        $result1 = mysqli_query($conn, $query1);
+        if(mysqli_num_rows($result1) > 0){    
+          while($row1 = mysqli_fetch_array($result1)){
+            $prod_id = $row1['prod_id'];
+            $query2 = "SELECT prod_cost FROM product WHERE prod_id = '$prod_id'";    
+            $result2 = mysqli_query($conn, $query2);
+            if(mysqli_num_rows($result2) > 0){    
+              while($row2 = mysqli_fetch_array($result2)){
+                $cost = $cost + $row2['prod_cost'];               
+              }   
+            }  
+          }   
+        }
+        $row['count'] = $cost;
         $output[] = $row;  
       }   
     }
