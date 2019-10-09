21a8cbda3cec60e440f889e54e5ba26b2d991177
mewole <37533052+mewole@users.noreply.github.com>
2019-10-09 20:06:15 +0700

    apk

---
+++ platforms/ios/www/php/get_all_cost.php
@@ -0,0 +1,46 @@
+<?php
+    include "db.php";
+    $cost = 0;
+
+    $sql = "SELECT * FROM warehouse WHERE amount > '0'"; 
+    $result = mysqli_query($conn, $sql);   
+  
+    if(mysqli_num_rows($result) > 0){    
+        while($row = mysqli_fetch_array($result)){  
+            $prod_id = $row['prod_id'];
+            $amount = $row['amount'];
+
+            $sql1 = "SELECT prod_cost FROM product WHERE prod_id = '$prod_id'"; 
+            $result1 = mysqli_query($conn, $sql1);
+            if(mysqli_num_rows($result1) > 0){    
+                while($row1 = mysqli_fetch_array($result1)){  
+                    $prod_cost = $row1['prod_cost'];
+                    $cost = $cost + ($prod_cost*$amount);
+                }
+            }
+                 
+        }
+    }
+
+    $sql = "SELECT * FROM shop WHERE amount > '0'"; 
+    $result = mysqli_query($conn, $sql);   
+  
+    if(mysqli_num_rows($result) > 0){    
+        while($row = mysqli_fetch_array($result)){  
+            $prod_id = $row['prod_id'];
+            $amount = $row['amount'];
+
+            $sql1 = "SELECT prod_cost FROM product WHERE prod_id = '$prod_id'"; 
+            $result1 = mysqli_query($conn, $sql1);
+            if(mysqli_num_rows($result1) > 0){    
+                while($row1= mysqli_fetch_array($result1)){  
+                    $prod_cost = $row1['prod_cost'];
+                    $cost = $cost + ($prod_cost*$amount);
+                }
+            }
+                 
+        }
+    }     
+    echo $cost; 
+    mysqli_close($conn);
+?>
