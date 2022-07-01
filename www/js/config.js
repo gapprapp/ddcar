cd2f712f6a98e98c549096dd943ee048d8fb91af
mewole <37533052+mewole@users.noreply.github.com>
2022-07-01 21:00:41 +0700

    Fix config to dynamic and add click full image

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,14 @@
 function getConfig(){
     //PRD
     localStorage.pathServer = "http://ddaccessory.trueddns.com:24330/dd-shop/"
-    
-    //TEST
-    //localStorage.pathServer = "http://ddaccessory.trueddns.com:24330/dd-shop-test/"
-    //document.cookie = "pathServer=http://ddaccessory.trueddns.com:24330/dd-shop/";
+
+    //User config
+    //logo
+    imgPath = "img/logo.jpg"
+    //for a4
+    shopDestination = "รถบริษัท DD MOTOR SPORT B-24, B-28 อาคารศรีวรจักร ถ.หลวงป้อมปราบศัตรูพ่าย"
+    //for bill
+    billshopName = "DD MOTOR SPORT"
+    billshopDestination = "B-24, B-28 อาคารศรีวรจักร ถ.หลวงป้อมปราบศัตรูพ่าย"
+    billshopTel = "โทร. 02-2265280 , 02-2222437"    
 }
